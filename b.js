5fb050f89b2fa6b442e83eefc7c43ff4ffc0ddc5
STLers <1427329584@qq.com>
2021-09-13 19:01:37 +0800

    add axios

--- b.js
+++ b.js
@@ -1,3 +1,18 @@
 var a = 1
-var b = 2
+var b = 2.0
 var c = 3
+import axios from 'axios'
+const instance = axios.create({
+    baseURL: 'xxx',
+    timeout: 100
+})
+instance.interceptors.request.use(req => {
+    return req
+}, err => {
+    return Promise.reject(err)
+})
+instance.interceptors.response.use(res => {
+    return res
+}, err => {
+    return Promise.reject(err)
+})
